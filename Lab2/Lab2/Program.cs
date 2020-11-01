49e9ae7955bb396d6e27104b190c40ccb3690571
jay.zupancic07@gmail.com <jay.zupancic07@gmail.com>
2020-11-01 11:24:45 -0500

    Lab2 Completion

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Multiplication
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int sum;
+
+            Console.WriteLine("Enter first integer: 15");
+            a = 15;
+
+            Console.WriteLine("Enter second integer: 20");
+            b = 20;
+
+            sum = a * b;
+
+            Console.WriteLine("Sum is {0}", sum);
+
+
+            Console.WriteLine("Hello World!\nfrom Jay");
+            Console.WriteLine("Hello World!\tFrom Jay");
+
+            //The main method is the point where execution starts in a C# console application
+
+            //The difference between an integer type variable and a double/floating point variable is that an integer type variable is a number with no decimal points and a floating point variable is a decimal variable.
+
+            //An example of a method in chapter 4 is Console.WriteLine method. A Console.WriteLine method requires a specific argument or statement of data and displays it in a console window. For exmaple, Console.WriteLine("Hello World!)" will play the message Hello World! in a console window.
+                      
+            //The get accessor reads the value and set accessor assigns a value.
+
+            //The difference between a class and an object is that an object is created from a class declaration. Each class that is created is a new type that one can use to create objects in it. There should be no limit as to how many instances of a class can be created. Im sure you can create as much as your memory can hold.
+        }
+
+        
+
+
+
+
+    }
+}
